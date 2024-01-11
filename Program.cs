2090f4d1337cb504e90bcac8d362d638695bb16f
Dimitar Dzhambazov <129491664+dzambazov1986@users.noreply.github.com>
2024-01-11 13:23:44 +0200

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,4 +1,8 @@
-﻿string townName = Console.ReadLine();
+﻿string firstName = Console.ReadLine();
+string lastName = Console.ReadLine();
+string email = Console.ReadLine();
+string phone = Console.ReadLine();
 string country = Console.ReadLine();
+string city = Console.ReadLine();
 
-Console.WriteLine($"{townName} is in {country}");
+Console.WriteLine($"{firstName} {lastName} with email adress {email} and phone Number {phone} from {country} living in {city}");
