d9c646afd8610739ba64c70b0c9b1810bcacedf8
Dimitar Dzhambazov <129491664+dzambazov1986@users.noreply.github.com>
2024-01-13 15:35:54 +0200

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,8 +1,3 @@
-﻿string firstName = Console.ReadLine();
-string lastName = Console.ReadLine();
-string email = Console.ReadLine();
-string phone = Console.ReadLine();
-string country = Console.ReadLine();
-string city = Console.ReadLine();
-
-Console.WriteLine($"{firstName} {lastName} with email adress {email} and phone Number {phone} from {country} living in {city}");
+﻿int side = int.Parse(Console.ReadLine());
+int area = side * side;
+Console.WriteLine(area);
